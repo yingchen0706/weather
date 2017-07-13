eb6c8015aec487bd0d8d41b1c7ebe924f2ee0eb9
yingchen0706 <yingchen0706@gmail.com>
2017-07-13 17:08:37 +0800

    weather project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+
 const API_KEY='eec4b6de34f9cc6f1539522186eebca0';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},cn`;
+    var request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
